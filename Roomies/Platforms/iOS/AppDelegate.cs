5d42bdc41ae28b4f704477a1dd949cd59713263d
Ciocmarean Cristian <cristianmircea19@gmail.com>
2026-02-28 18:39:25 +0200

    Add project files.

---
+++ Roomies/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Roomies
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
